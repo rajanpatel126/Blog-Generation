3adce43c8219c22abf2c35f2001237fa4cd61a01
Rajan Patel <88902043+rajanpatel126@users.noreply.github.com>
2024-02-19 23:20:05 +0530

    Response Generation complete using Mixtral

--- app.py
+++ app.py
@@ -1,60 +1,64 @@
-# import streamlit as st
-# import os
-# from langchain.prompts import PromptTemplate
-# from langchain_community.llms import HuggingFaceHub
-# from langchain.chains import LLMChain
+import streamlit as st
+import os
+from langchain.prompts import PromptTemplate
+from langchain_community.llms import HuggingFaceHub
+from langchain.chains import LLMChain
+from dotenv import load_dotenv
 
-# os.environ["HUGGINGFACEHUB_API_TOKEN"] = os.getenv("HUGGINGFACEHUB_API_TOKEN")
+load_dotenv()
+os.getenv("HUGGINGFACEHUB_API_TOKEN")
 
-# # Get response from llama-2
-# def getLLamaResponse(input_text, no_words, blog_style):
+# Get response from model
+def getResponse(input_text, no_words, blog_style):
 
-#     # Model calling
-#     llm = HuggingFaceHub(repo_id="daryl149/llama-2-7b-chat-hf", model_kwargs={"temperature":0.8, "max_length":4096,"max_new_tokens":4096, "task": "text-generation"})
+    # Model calling
+    llm = HuggingFaceHub(repo_id="mistralai/Mixtral-8x7B-Instruct-v0.1", model_kwargs={"temperature":0.8, "max_length":4096,"max_new_tokens":4096})
 
-#     # Prompt Template
-#     template = """
-#     You are the writing editor which can write the blog. 
-#     You have to write the blog on {input_text} of word count of {no_words} in such a manner that any topic should be described in form of a story with examples which resonates with the topic.
-#     Stick with the topic and try to deliver the message from a blog for the described target audience which is {blog_style}. 
-#     Make the whole blog SEO friendly.
-#     If the topic is illegal, harmful or vulgure then just respond that "I can not assist you with the illegal, harmful topic. Seeking the information of such topic could indiate a harmful intent."
-#     """
+    # Prompt Template
+    template = """
 
-#     prompt = PromptTemplate(template=template,
-#                             input_variables=['input_text', 'no_words','blog_style'])
-#     chain = LLMChain(llm=llm, prompt=prompt, verbose=True)
-#     # Generating the response from llama-2
-#     # response = llm.invoke(prompt.format(input_text=input_text, no_words=no_words, blog_style=blog_style))
-#     response = chain.invoke({'input_text': input_text, 'no_words': no_words, 'blog_style':blog_style})
-#     print(response)
+You are the writing editor and can write the blog as if you are an experienced writer.
 
-#     return response
+Write an article on "{input_text}" of approximately {no_words} words. Describe the topic as a story with examples that resonate with the reader. Stay on topic and deliver the message in a blog format suitable for the target audience: {blog_style}.
+
+Make the blog SEO-friendly.
+
+If the topic is illegal, harmful, or vulgar, respond with "I cannot assist you with illegal, harmful topics. Seeking information on such topics could indicate harmful intent."
+
+"""
 
 
-# st.set_page_config(page_title='Blog Generation',
-#                    page_icon='🤖',
-#                    layout='centered',
-#                    initial_sidebar_state='collapsed')
+    prompt = PromptTemplate(template=template,
+                            input_variables=['input_text', 'no_words','blog_style'])
+    chain = LLMChain(llm=llm, prompt=prompt, verbose=True)
+    
+    response = chain.invoke({'input_text': input_text, 'no_words': no_words, 'blog_style':blog_style})
+    # print(response)
 
-# st.header('GenX Blogs 🤖')
+    return response['text']
 
-# input_text = st.text_input("Enter your blog topic")
 
-# # Creating two more additional fields
-# col1, col2 = st.columns([5, 5])
+st.set_page_config(page_title='Blog Generation',
+                   page_icon='🤖',
+                   layout='centered',
+                   initial_sidebar_state='collapsed')
 
-# with col1:
-#     no_words = st.text_input('No of words: ')
-# with col2:
-#     blog_style = st.selectbox('Target Audience for the blog: ',
-#                               ('Professionals', 'Academics', 'General Public', 'Undergraduate', 'k-12'),
-#                               index=2)
+st.header('GenX Blogs 🤖')
 
-# submit = st.button('Generate')
+input_text = st.text_input("Enter your blog topic")
 
-# # Output
-# if submit:
-#     st.write(getLLamaResponse(input_text, no_words, blog_style))
+# Creating two more additional fields
+col1, col2 = st.columns([5, 5])
 
-print('Hello World')
+with col1:
+    no_words = st.text_input('No of words: ')
+with col2:
+    blog_style = st.selectbox('Target Audience for the blog: ',
+                              ('Professionals', 'Academics', 'General Public', 'Undergraduate', 'k-12'),
+                              index=2)
+
+submit = st.button('Generate')
+
+# Output
+if submit:
+    st.write(getResponse(input_text, no_words, blog_style))
