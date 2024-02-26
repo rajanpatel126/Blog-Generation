579e8395b6b2f5a41be357a6441b478651c07cd7
Rajan Patel <88902043+rajanpatel126@users.noreply.github.com>
2024-02-26 13:49:21 +0530

    Combined two functionalities by creating the object of search in generation file.

---
+++ googleSearch.py
@@ -0,0 +1,29 @@
+import os
+from langchain_community.utilities import GoogleSearchAPIWrapper
+from dotenv import load_dotenv
+from langchain.tools import Tool
+load_dotenv()
+
+os.environ["GOOGLE_CSE_ID"] = os.getenv('GOOGLE_CSE_ID')
+os.environ["GOOGLE_API_KEY"] = os.getenv('GOOGLE_API_KEY')
+
+class GoogleSearch():
+    tool=None
+    
+    def make_model(self):
+
+        # Tool integration
+        def top5_results(query):
+            search = GoogleSearchAPIWrapper()
+            return search.results(query, 5)
+
+        GoogleSearch.tool = Tool(
+            name="Google Search Snippets",
+            description="Search Google for top results.",
+            func=top5_results,
+        )
+        
+    def ask_query(self,query:str):
+        if GoogleSearch.tool is None: 
+            self.make_model()
+        return GoogleSearch.tool.run(query)
